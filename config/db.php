404c75cb681f895d09ffd07d2dc9df5da63ad4b7
Sergeygeek <sukhorukov1986@yandex.ru>
2019-05-08 00:11:24 +0400

    app added Procfile, htaccess and modified config db

--- config/db.php
+++ config/db.php
@@ -1,14 +1,17 @@
 <?php
+if (getenv("DATABASE_URL")){
+    $url = parse_url(getenv("DATABASE_URL"));
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => "mysql:host={$url["host"]};dbname=" . substr($url["path"], 1),
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
+    ];
+}
